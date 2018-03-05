0f7f6bb98a71bd6e480bc9422fb940309bffc834
Henrik Heggelund-Berg <hhe@dips.no>
2018-03-05 12:18:20 +0100

    Fix whitespace as delimiter in find pattern

--- scripts/nameHack.sh
+++ scripts/nameHack.sh
@@ -8,7 +8,7 @@ _replace_filenames()
     old2=$4
     new=$5
     echo "Replacing $old1$separator$old2 -> $new in file/folder names"
-    find $1 -depth -not -path '*/\.*' -name "*$old1$separator$old2*" -execdir bash -c "rename 's/$old1\\$separator$old2/$new/' *" -- {} +
+    find $1 -depth -not -path '*/\.*' -name "*$old1$separator$old2*" -execdir bash -c "rename 's/$old1$separator$old2/$new/' *" -- {} +
 }
 
 # Usage: _replace_filenames rootPath old1 separator old2 newString
@@ -19,7 +19,7 @@ _replace_filecontent()
     old2=$4
     new=$5
     echo "Replacing $old1$separator$old2 -> $new in file content"
-    find $1 -not -path '*/\.*' -type f -exec sed -i "s/$old1\\$separator$old2/$new/g" {} +
+    find $1 -not -path '*/\.*' -type f -exec sed -i "s/$old1$separator$old2/$new/g" {} +
 }
 
 # Usage: _lowercase "STRING"
@@ -35,11 +35,11 @@ _replace_combinations()
     oldPattern=(${2//./ });
     newPattern=(${3//./ });
 
-    delimiters=".;-; " # separated by ';'
+    delimiters="\.;\-; " # separated by ';'
     IFS=';' read -ra ADDR <<< "$delimiters"
     for i in "${ADDR[@]}"; do
-        _replace_filenames $rootPath ${oldPattern[0]} $i ${oldPattern[1]} "${newPattern[0]}$i${newPattern[1]}";
-        _replace_filecontent $rootPath ${oldPattern[0]} $i ${oldPattern[1]} "${newPattern[0]}$i${newPattern[1]}";
+        _replace_filenames $rootPath ${oldPattern[0]} "$i" ${oldPattern[1]} "${newPattern[0]}$i${newPattern[1]}";
+        _replace_filecontent $rootPath ${oldPattern[0]} "$i" ${oldPattern[1]} "${newPattern[0]}$i${newPattern[1]}";
     done
 }
 
